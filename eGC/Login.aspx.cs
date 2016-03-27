7107b4adaea7d346da07d20a7ac9300339a55ba8
ismael-desktop <ismaeljaycarpio1992@gmail.com>
2016-03-28 01:55:45 +0800

    modified some content

--- eGC/Login.aspx.cs
+++ eGC/Login.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +12,11 @@ namespace eGC
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(!Page.IsPostBack)
+            {
+                Session.RemoveAll();
+                FormsAuthentication.SignOut();
+            }
         }
     }
 }
